ea9f6050062fc6d1f8840df2769103d19025e48d
Marcelo Vergara <marcelorv@gmail.com>
2023-04-21 08:09:40 -0300

    tag management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,20 +12,21 @@ export default function App({ Component, pageProps }: AppProps) {
           name="description"
           content="Fabula Infantil - um site interativo para crianças onde elas podem personalizar e criar histórias incríveis! Com a ajuda de inteligência artificial, oferecemos aventuras únicas e empolgantes para estimular a imaginação e promover o amor pela leitura."
         />
-      </Head>
-      {/* <!-- Google tag (gtag.js) --> */}
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-2YQKQQP4ZX"></Script>
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2YQKQQP4ZX"
+        />
+        <script id="google-analytics">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
           gtag('config', 'G-2YQKQQP4ZX');
         `}
-      </Script>
+        </script>
+      </Head>
+      {/* <!-- Google tag (gtag.js) --> */}
       <Component {...pageProps} />
 
       <Analytics />
